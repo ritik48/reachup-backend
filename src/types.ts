5d40f025f2bf0a05a50ab1eb025b19c73c4a972c
Ritik <raj76941@gmail.com>
2025-03-30 18:44:45 +0530

    feat: added type for email, leads

--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,32 @@ export interface IUser extends Document {
   email: string;
   password?: string;
 }
+
+export interface ILead extends Document {
+  _id: mongoose.Types.ObjectId;
+  title: string;
+  user: mongoose.Types.ObjectId;
+}
+
+export interface ILeadItem extends Document {
+  _id: mongoose.Types.ObjectId;
+  leadId: mongoose.Types.ObjectId;
+  email: string;
+  firstname?: string;
+  lastname?: string;
+  company?: string;
+  status: {
+    type: String;
+    enum: ["pending", "sent", "failed"];
+    default: "pending";
+  };
+  emailSentAt?: Date;
+}
+
+export interface IEmailProvider extends Document {
+  _id: mongoose.Types.ObjectId;
+  provider: string;
+  email: string;
+  access_token: string;
+  refresh_token: string;
+}
