0c916ff47803f239dd5cd8590b68c545075ce757
Ritik <raj769417@gmail.com>
2025-04-05 23:40:29 +0530

    fix env

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.use("/uploads", express.static("uploads"));
 
 app.use(
   cors({
-    origin: process.env.BACKEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "DELETE", "PATCH"],
   })
